28c4c51c4f8864f8350bdf522ef626390baff9e2
unknown <diego170975@gmail.com>
2021-02-05 17:27:44 +0000

    added blocks

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
+// const collapsibles = document.querySelectorAll(".collapsible");
+// collapsibles.forEach((item) =>
+//   item.addEventListener("click", function () {
+//     this.classList.toggle("collapsible--expanded");
+//   })
+// );
+
 const collapsibles = document.querySelectorAll(".collapsible");
 collapsibles.forEach((item) =>
   item.addEventListener("click", function () {
-    this.classList.toggle("collapsible--expanded");
+    item.classList.toggle("collapsible--expanded");
   })
 );
