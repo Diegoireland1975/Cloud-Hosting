99ffad05ab860fade7381dce41524921e2abadc7
unknown <diego170975@gmail.com>
2021-02-11 11:08:01 +0000

    added hero  domain and footer section

--- js/main.js
+++ js/main.js
@@ -1,13 +1,6 @@
-// const collapsibles = document.querySelectorAll(".collapsible");
-// collapsibles.forEach((item) =>
-//   item.addEventListener("click", function () {
-//     this.classList.toggle("collapsible--expanded");
-//   })
-// );
-
 const collapsibles = document.querySelectorAll(".collapsible");
 collapsibles.forEach((item) =>
   item.addEventListener("click", function () {
-    item.classList.toggle("collapsible--expanded");
+    this.classList.toggle("collapsible--expanded");
   })
 );
